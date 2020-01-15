f22604b4c245adac7cdfcab08e5cea3bb592e047
rabah babaci <4rabah@gmail.com>
2020-01-14 21:03:02 -0800

    add digital timer version 1

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,27 @@
+let secTens = document.getElementById("secondTens");
+let secOnes = document.getElementById("secondOnes");
+
+let msTens = document.getElementById('msHundreds');
+let msOnes = document.getElementById('msTens');
+
+let clock = [secTens, secOnes, msTens, msOnes];
+
+function countUp() {
+    clock.forEach(elm => elm.textContent = 0);
+    let num = 0;
+    let time = setInterval(() => {
+        num++;
+        if (num >= 10) {
+            secTens.textContent = 1;
+            secOnes.textContent = 0;
+
+            clock.forEach(elem => elem.classList.add('redDigit'));
+            clearInterval(time);
+            console.log('Done');
+        } else {
+            secOnes.textContent = num;
+        }
+    }, 1000);
+}
+
+// countUp();
