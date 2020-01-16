df5ed8f3c553bcf7b7bcaae87c7c7063de058560
rabah babaci <4rabah@gmail.com>
2020-01-16 15:55:51 -0800

    fix digital timer and add some styling

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -6,6 +6,7 @@ let msOnes = document.getElementById('msTens');
 
 let clock = [secTens, secOnes, msTens, msOnes];
 
+// Manipulates the seconds
 function countUpSec() {
     let num = 0;
     clock.forEach(elm => elm.textContent = 0);
@@ -17,22 +18,44 @@ function countUpSec() {
             secOnes.textContent = 0;
 
             clock.forEach(elem => elem.classList.add('redDigit'));
+            document.body.style.background = '#447dc9';
             clearInterval(time);
-            console.log('Done');
         } else {
             secOnes.textContent = num;
         }
     }, 1000);
 }
 
-// countUpSec();
+// Manipulates the milliseconds
+function countUpMs() {
+    let num = 0;
+    let reset = 0;
 
+    let timing = setInterval(() => {
+        num++;
+        msTens.innerText = 0;
+
+        if (reset >= 10) {
+            msOnes.innerText = 0;
+            clearInterval(timing);
+        } else {
+            if (num == 10) {
+                msTens.innerText = 1;
+                num = 0;
+                reset++;
+            }
+            msOnes.innerText = num;
+        }
+    }, 100);
+}
+
+// Buttons Event listeners and handlers
 const buttons = document.getElementById('buttons');
 buttons.addEventListener('click', (event) => {
     if (event.target.id == 'start') {
         countUpSec();
-    } else {
+        countUpMs();
+    } else if (event.target.id == 'reset'){
         countUpSec();
-        event.preventDefault();
     }
-})
+});
